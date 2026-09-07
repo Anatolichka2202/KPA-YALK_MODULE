74ab1109880516ff9c48c1c329d055655b970be4
Anatolichka2202 <106554969+Anatolichka2202@users.noreply.github.com>
2026-09-07 16:56:50 +0300

    design: switch prototype entry to Station shell v1

--- design-prototype/src/main.jsx
+++ design-prototype/src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { App } from "./App.jsx";
-import "./styles.css";
+import { AppV1 } from "./AppV1.jsx";
+import "./station-v1.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <AppV1 />
   </React.StrictMode>,
 );
