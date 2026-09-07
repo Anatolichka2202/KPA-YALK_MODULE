ca4608951c984101afecba446f740fa6847ef871
Anatolichka2202 <106554969+Anatolichka2202@users.noreply.github.com>
2026-09-07 17:29:38 +0300

    design: load empty launcher styles

--- design-prototype/src/main.jsx
+++ design-prototype/src/main.jsx
@@ -7,6 +7,7 @@ import "./station-v1-scenarios.css";
 import "./station-v1-workspaces.css";
 import "./station-v1-edge.css";
 import "./station-v1-hmi-states.css";
+import "./station-v1-empty.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
