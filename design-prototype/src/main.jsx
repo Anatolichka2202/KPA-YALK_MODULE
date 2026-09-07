79997469abf9239e6b134059092ff6292c1e3ad3
Anatolichka2202 <106554969+Anatolichka2202@users.noreply.github.com>
2026-09-07 17:03:23 +0300

    design: load safety interaction overrides

--- design-prototype/src/main.jsx
+++ design-prototype/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { AppV1 } from "./AppV1.jsx";
 import "./station-v1.css";
+import "./station-v1-safety.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
