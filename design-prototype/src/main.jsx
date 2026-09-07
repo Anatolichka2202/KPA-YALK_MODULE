78c8fdf98ec68fd9aaf8c986e341a37620346e52
Anatolichka2202 <106554969+Anatolichka2202@users.noreply.github.com>
2026-09-07 17:15:15 +0300

    design: load scenario prototype styles

--- design-prototype/src/main.jsx
+++ design-prototype/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { AppV1 } from "./AppV1.jsx";
 import "./station-v1.css";
 import "./station-v1-safety.css";
+import "./station-v1-scenarios.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
