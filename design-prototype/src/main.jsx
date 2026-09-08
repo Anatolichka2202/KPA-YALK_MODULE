a386331663f280fc14d77209828d36f7d6c0cf2c
Anatolichka2202 <106554969+Anatolichka2202@users.noreply.github.com>
2026-09-08 16:44:06 +0300

    design: load UBSI prototype layout overrides

--- design-prototype/src/main.jsx
+++ design-prototype/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { UbsiSupplyApp } from "./ubsi/UbsiSupplyApp.jsx";
 import "./ubsi/ubsi-supply.css";
+import "./ubsi/ubsi-supply-overrides.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
