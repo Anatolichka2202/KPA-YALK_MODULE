1395b3fb3b52357b51c0617832d4ace3b3272383
Anatolichka2202 <106554969+Anatolichka2202@users.noreply.github.com>
2026-09-08 17:12:44 +0300

    fix(prototype): make corrected UBSI operator flow active

--- design-prototype/src/main.jsx
+++ design-prototype/src/main.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { UbsiSupplyApp } from "./ubsi/UbsiSupplyApp.jsx";
-import "./ubsi/ubsi-supply.css";
-import "./ubsi/ubsi-supply-overrides.css";
+import { UbsiOperatorApp } from "./ubsi/UbsiOperatorApp.jsx";
+import "./ubsi/ubsi-operator.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <UbsiSupplyApp />
+    <UbsiOperatorApp />
   </React.StrictMode>,
 );
