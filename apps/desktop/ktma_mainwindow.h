f335ecf59bccf8ea644a5b90c69ccfad40e72e63
Anatolichka2202 <106554969+Anatolichka2202@users.noreply.github.com>
2026-09-11 10:04:48 +0300

    production: forbid test-screen registration

--- apps/desktop/ktma_mainwindow.h
+++ apps/desktop/ktma_mainwindow.h
@@ -37,7 +37,6 @@ private:
     void restoreTuSelector();
     void applyProductionScenario();
     QString productionCodeForScope(const QString& scope) const;
-    bool registerProductionProduct(const QString& serial);
     void clearPendingProduction() noexcept;
 
     std::unique_ptr<ktma::ubsi::ProductionLedger> productionLedger_;
