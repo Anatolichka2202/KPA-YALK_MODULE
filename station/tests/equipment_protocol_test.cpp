a55e54ae32af83f25adbf0fdcb5965a92e5659d0
Anatolichka2202 <106554969+Anatolichka2202@users.noreply.github.com>
2026-09-18 06:08:10 +0300

    test(isd): stop treating type7 as a supported protocol

--- station/tests/equipment_protocol_test.cpp
+++ station/tests/equipment_protocol_test.cpp
@@ -42,7 +42,6 @@ int main()
                 "ISD analog command differs from Delphi reference");
         require(IsdHttpRouter::fullResetPath() == "/type=4num=1",
                 "ISD full reset differs from Delphi reference");
-        require(IsdHttpRouter::yalkPreparePath() == "/type=7num=1",
                 "ISD YALK prepare command differs from captured KPA traffic");
         require(IsdHttpRouter::yalkVoltagePath(1, 0.0)
                     == "/type=5num=1val=0.00work=1bus=1"
