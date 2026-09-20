c12bb97cf024b57203c8a8caf357be51ad6f80bf
Anatolichka2202 <106554969+Anatolichka2202@users.noreply.github.com>
2026-09-20 17:04:02 +0300

    test: cover runtime journal widget in UI smoke

--- tests/ui_smoke.cpp
+++ tests/ui_smoke.cpp
@@ -1,3 +1,4 @@
+#include "ui/run_journal_overlay.h"
 #include "ui/test_page.h"
 
 #include <QApplication>
@@ -7,5 +8,9 @@ int main(int argc, char* argv[])
     QApplication app(argc, argv);
     TestPage page;
     page.setProductionMode(false);
+
+    RunJournalOverlay journal(&page);
+    journal.beginRun();
+    journal.finishRun();
     return 0;
 }
