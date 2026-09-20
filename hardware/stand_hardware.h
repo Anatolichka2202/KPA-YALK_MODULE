85673400684aa0468eb31dee715400cbbe782d10
Anatolichka2202 <106554969+Anatolichka2202@users.noreply.github.com>
2026-09-20 12:51:34 +0300

    hardware: own clean full stand instruments

--- hardware/stand_hardware.h
+++ hardware/stand_hardware.h
@@ -1,9 +1,12 @@
 #pragma once
 
 #include "hardware/akip1160.h"
+#include "hardware/bench_instruments.h"
+#include "hardware/isd_router.h"
 #include "hardware/stand_config.h"
 #include "hardware/yalk_reference_link.h"
 
+#include <memory>
 #include <string>
 
 namespace tu::hardware {
@@ -17,16 +20,25 @@ public:
     StandHardware& operator=(const StandHardware&) = delete;
 
     std::string probeSupplyCold();
+    std::string probeIsd();
+    std::string probeV7();
+    std::string probeGenerator();
     void safeStop() noexcept;
 
     Akip1160& supply() noexcept { return supply_; }
     YalkReferenceLink& yalk() noexcept { return yalk_; }
+    IsdRouter& isd() noexcept { return isd_; }
+    V7Meter& v7();
+    RigolGenerator& generator();
     const StandConfig& config() const noexcept { return config_; }
 
 private:
     StandConfig config_;
     Akip1160 supply_;
     YalkReferenceLink yalk_;
+    IsdRouter isd_;
+    std::unique_ptr<V7Meter> v7_;
+    std::unique_ptr<RigolGenerator> generator_;
 };
 
 } // namespace tu::hardware
