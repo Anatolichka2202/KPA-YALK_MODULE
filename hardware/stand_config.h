f14e15159c91133b25a746456452e3f2e94ce356
Anatolichka2202 <106554969+Anatolichka2202@users.noreply.github.com>
2026-09-20 12:46:10 +0300

    hardware: extend clean stand profile for full tract

--- hardware/stand_config.h
+++ hardware/stand_config.h
@@ -3,6 +3,7 @@
 #include <cstdint>
 #include <filesystem>
 #include <string>
+#include <vector>
 
 namespace tu::hardware {
 
@@ -27,9 +28,31 @@ struct YalkUdpConfig {
     unsigned receiveTimeoutMilliseconds = 800;
 };
 
+struct IsdConfig {
+    std::string host;
+    std::uint16_t port = 80;
+    unsigned timeoutMilliseconds = 1500;
+};
+
+struct V7Config {
+    std::vector<std::string> resourceExpressions;
+    unsigned timeoutMilliseconds = 5000;
+    std::string dcVoltageCommand = "MEAS:VOLT:DC?";
+    std::string acVoltageCommand = "MEAS:VOLT:AC?";
+    std::string frequencyCommand = "MEAS:FREQ?";
+};
+
+struct RigolConfig {
+    std::vector<std::string> resourceExpressions;
+    unsigned timeoutMilliseconds = 2000;
+};
+
 struct StandConfig {
     AkipConfig supply;
     YalkUdpConfig yalk;
+    IsdConfig isd;
+    V7Config v7;
+    RigolConfig generator;
 };
 
 StandConfig loadStandConfig(const std::filesystem::path& path);
