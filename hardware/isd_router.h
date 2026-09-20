d648cb5a1131938fea33ec9a6f64087a11bb8c88
Anatolichka2202 <106554969+Anatolichka2202@users.noreply.github.com>
2026-09-20 12:43:58 +0300

    hardware: add direct ISD HTTP router

---
+++ hardware/isd_router.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "hardware/stand_config.h"
+
+#include <memory>
+#include <string>
+
+namespace tu::hardware {
+
+class IsdRouter final
+{
+public:
+    explicit IsdRouter(IsdConfig config);
+    ~IsdRouter();
+
+    IsdRouter(const IsdRouter&) = delete;
+    IsdRouter& operator=(const IsdRouter&) = delete;
+
+    std::string probe();
+    void setSwitch(unsigned type, unsigned channel, bool enabled);
+    void setAnalog(unsigned channel, unsigned value, bool enabled);
+    void setYalkVoltage(unsigned channel, double volts);
+    void disableYalkOutput(unsigned channel);
+
+private:
+    struct Impl;
+    std::unique_ptr<Impl> impl_;
+};
+
+} // namespace tu::hardware
