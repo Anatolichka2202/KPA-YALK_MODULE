039727b1d11d9c7ccbbcba1363dee0245898d2ed
Anatolichka2202 <106554969+Anatolichka2202@users.noreply.github.com>
2026-09-20 12:50:53 +0300

    hardware: expose ISD startup baseline

--- hardware/isd_router.h
+++ hardware/isd_router.h
@@ -17,6 +17,7 @@ public:
     IsdRouter& operator=(const IsdRouter&) = delete;
 
     std::string probe();
+    void serviceFullReset();
     void setSwitch(unsigned type, unsigned channel, bool enabled);
     void setAnalog(unsigned channel, unsigned value, bool enabled);
     void setYalkVoltage(unsigned channel, double volts);
