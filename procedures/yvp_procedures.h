0334efa94f118d9cfd7d4cc049782b0015f9a569
Anatolichka2202 <106554969+Anatolichka2202@users.noreply.github.com>
2026-09-20 12:54:50 +0300

    procedures: declare clean YVP tract

---
+++ procedures/yvp_procedures.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "backend/scenario_engine.h"
+
+#include <memory>
+
+namespace tu::hardware { class StandHardware; }
+
+namespace tu::procedures {
+
+void registerYvpProcedures(ScenarioEngine& engine,
+                           std::shared_ptr<hardware::StandHardware> hardware);
+
+} // namespace tu::procedures
